530902725e02aacdec63a461ecd4a81cd7dba423
Trickey <vngthnh999@gmail.com>
2022-07-28 07:04:22 +0700

    Completed animate-scroll exercises

---
+++ exercises/animate-scroll/animate-scroll.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".container img");
+  window.addEventListener("scroll", function () {
+    const windowScrollTop = window.pageYOffset;
+    console.log(windowScrollTop);
+    [...images].forEach((item) => {
+      if (windowScrollTop > item.offsetTop - item.offsetHeight / 2) {
+        item.classList.add("active");
+      }
+    });
+  });
+});
